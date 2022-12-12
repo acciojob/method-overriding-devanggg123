5521938f12c690149e78809571923edb5d47d4c0
devanggg123 <114430661+devanggg123@users.noreply.github.com>
2022-12-12 17:26:06 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
   public static class A {
       public String meth(){
-          return "Invoking method form class A";
+          return "Invoking method from class A";
       }
   }
   public static class B extends A{
@@ -11,7 +11,7 @@ public class Main {
 
       @Override
       public String meth(){
-          return "Method is overridden in Extended class B";
+          return "Method is overridden in Extendend class B";
       }
   }
   public static void  main(String[] args){
