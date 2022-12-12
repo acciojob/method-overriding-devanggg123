b9506cc258060242acb95f4eceba2ef91a63e219
devanggg123 <sfdc.devang123@gmail.com>
2022-12-12 17:15:47 +0530

    hogya

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+  public static class A {
+      public String meth(){
+          return "Invoking method form class A";
+      }
+  }
+  public static class B extends A{
+
+
+      @Override
+      public String meth(){
+          return "Method is overridden in Extended class B";
+      }
+  }
+  public static void  main(String[] args){
+      A obj=new B();
+      obj.meth();
+  }
 }
